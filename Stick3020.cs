984e62abfbfcd2fc0d6f838ed2a515d0932bd0bf
MikeCAT <mikecat@users.noreply.github.com>
2024-08-21 09:02:49 +0900

    add language menu and move some text setting to a method

--- Stick3020.cs
+++ Stick3020.cs
@@ -36,6 +36,10 @@ class Stick3020: Form
 
 	private UIText uiText = new JapaneseUIText();
 
+	private MenuStrip mainMenuStrip;
+	private ToolStripMenuItem languageMenuItem;
+	private ToolStripMenuItem languageJapaneseMenuItem, languageEnglishMenuItem;
+
 	private Panel mainPanel;
 
 	private GroupBox controllerSelectGroupBox;
@@ -73,71 +77,71 @@ class Stick3020: Form
 		this.Text = "Stick 3020";
 		SuspendLayout();
 
+		mainMenuStrip = new MenuStrip();
+		languageMenuItem = new ToolStripMenuItem();
+		languageMenuItem.Text = "言語 / Language (&L)";
+		languageJapaneseMenuItem = new ToolStripMenuItem();
+		languageJapaneseMenuItem.Text = "日本語 (&J)";
+		languageJapaneseMenuItem.Checked = true;
+		languageEnglishMenuItem = new ToolStripMenuItem();
+		languageEnglishMenuItem.Text = "English (&E)";
+		languageMenuItem.DropDownItems.Add(languageJapaneseMenuItem);
+		languageMenuItem.DropDownItems.Add(languageEnglishMenuItem);
+		mainMenuStrip.Items.Add(languageMenuItem);
+		this.Controls.Add(mainMenuStrip);
+		this.MainMenuStrip = mainMenuStrip;
+
 		mainPanel = CreateControl<Panel>(this, 0, 0, 26.5f, 13.5f);
+		mainPanel.Top += mainMenuStrip.Height;
 		this.ClientSize = mainPanel.Size;
+		this.Height += mainMenuStrip.Height;
 		mainPanel.SuspendLayout();
 
 		controllerSelectGroupBox = CreateControl<GroupBox>(mainPanel, 0.5f, 0.5f, 10, 5.5f);
-		controllerSelectGroupBox.Text = uiText.ControllerSelect;
 		controllerSelectGroupBox.SuspendLayout();
 		for (int i = 0; i < 4; i++)
 		{
 			controllerSelectRadioButtons[i] = CreateControl<RadioButton>(
 				controllerSelectGroupBox, 0.5f, i + 1, 9, 1
 			);
-			controllerSelectRadioButtons[i].Text = i.ToString();
 		}
 		controllerSelectGroupBox.ResumeLayout();
 
 		brakeSelectGroupBox = CreateControl<GroupBox>(mainPanel, 0.5f, 6.5f, 10, 6.5f);
-		brakeSelectGroupBox.Text = uiText.BrakeSelect;
 		brakeSelectGroupBox.SuspendLayout();
 		brakeAutoRadioButton = CreateControl<RadioButton>(brakeSelectGroupBox, 0.5f, 1, 9, 1);
-		brakeAutoRadioButton.Text = string.Format("{0}：{1}", uiText.BrakeAuto, uiText.Brake6StepForAuto);
 		brake6StepRadioButton = CreateControl<RadioButton>(brakeSelectGroupBox, 0.5f, 2, 9, 1);
-		brake6StepRadioButton.Text = uiText.Brake6Step;
 		brake7StepRadioButton = CreateControl<RadioButton>(brakeSelectGroupBox, 0.5f, 3, 9, 1);
-		brake7StepRadioButton.Text = uiText.Brake7Step;
 		brake8StepRadioButton = CreateControl<RadioButton>(brakeSelectGroupBox, 0.5f, 4, 9, 1);
-		brake8StepRadioButton.Text = uiText.Brake8Step;
 		brakeAnalogRadioButton = CreateControl<RadioButton>(brakeSelectGroupBox, 0.5f, 5, 9, 1);
-		brakeAnalogRadioButton.Text = uiText.BrakeAnalog;
 		brakeSelectGroupBox.ResumeLayout();
 
 		operationStatusGroupBox = CreateControl<GroupBox>(mainPanel, 11, 0.5f, 15, 5.5f);
-		operationStatusGroupBox.Text = uiText.OperationStatus;
 		operationStatusGroupBox.SuspendLayout();
 		powerTitleLabel = CreateControl<Label>(operationStatusGroupBox, 0.5f, 1, 5, 1);
-		powerTitleLabel.Text = uiText.OperationPower;
 		powerNotchLabel = CreateControl<Label>(operationStatusGroupBox, 5.5f, 1, 5, 1);
-		powerNotchLabel.Text = string.Format("{0}{1}", uiText.OperationPowerHolding, 1);
+		powerNotchLabel.Text = "N";
 		powerDepthLabel = CreateControl<Label>(operationStatusGroupBox, 10.5f, 1, 4, 1);
-		powerDepthLabel.TextAlign = ContentAlignment.TopRight;
 		powerDepthLabel.Text = "100%";
 		brakeTitleLabel = CreateControl<Label>(operationStatusGroupBox, 0.5f, 2, 5, 1);
-		brakeTitleLabel.Text = uiText.OperationBrake;;
 		brakeNotchLabel = CreateControl<Label>(operationStatusGroupBox, 5.5f, 2, 5, 1);
 		brakeNotchLabel.Text = "400kPa";
 		brakeDepthLabel = CreateControl<Label>(operationStatusGroupBox, 10.5f, 2, 4, 1);
 		brakeDepthLabel.TextAlign = ContentAlignment.TopRight;
 		brakeDepthLabel.Text = "100%";
 		leftTriggerTitleLabel = CreateControl<Label>(operationStatusGroupBox, 0.5f, 3, 10, 1);
-		leftTriggerTitleLabel.Text = uiText.OperationLeftTrigger;
 		leftTriggerDepthLabel = CreateControl<Label>(operationStatusGroupBox, 10.5f, 3, 4, 1);
 		leftTriggerDepthLabel.TextAlign = ContentAlignment.TopRight;
 		leftTriggerDepthLabel.Text = "0%";
 		rightTriggerTitleLabel = CreateControl<Label>(operationStatusGroupBox, 0.5f, 4, 10, 1);
-		rightTriggerTitleLabel.Text = uiText.OperationRightTrigger;
 		rightTriggerDepthLabel = CreateControl<Label>(operationStatusGroupBox, 10.5f, 4, 4, 1);
 		rightTriggerDepthLabel.TextAlign = ContentAlignment.TopRight;
 		rightTriggerDepthLabel.Text = "0%";
 		operationStatusGroupBox.ResumeLayout();
 
 		inputConfigurationGroupBox = CreateControl<GroupBox>(mainPanel, 11, 6.5f, 15, 6.5f);
-		inputConfigurationGroupBox.Text = uiText.InputConfiguration;
 		inputConfigurationGroupBox.SuspendLayout();
 		stickThresholdTitleLabel = CreateControl<Label>(inputConfigurationGroupBox, 0.5f, 1, 9, 1);
-		stickThresholdTitleLabel.Text = uiText.InputStickThreshold;
 		stickThresholdNumericUpDown = CreateControl<NumericUpDown>(inputConfigurationGroupBox, 9.5f, 1, 4, 1);
 		stickThresholdNumericUpDown.Maximum = 100;
 		stickThresholdNumericUpDown.Minimum = 0;
@@ -146,7 +150,6 @@ class Stick3020: Form
 		stickThresholdUnitLabel = CreateControl<Label>(inputConfigurationGroupBox, 13.5f, 1, 1, 1);
 		stickThresholdUnitLabel.Text = "%";
 		trigterShallowThresholdTitleLabel = CreateControl<Label>(inputConfigurationGroupBox, 0.5f, 2, 9, 1);
-		trigterShallowThresholdTitleLabel.Text = uiText.InputTriggerThresholdShallow;
 		trigterShallowThresholdNumericUpDown = CreateControl<NumericUpDown>(inputConfigurationGroupBox, 9.5f, 2, 4, 1);
 		trigterShallowThresholdNumericUpDown.Maximum = 80;
 		trigterShallowThresholdNumericUpDown.Minimum = 0;
@@ -155,7 +158,6 @@ class Stick3020: Form
 		trigterShallowThresholdUnitLabel = CreateControl<Label>(inputConfigurationGroupBox, 13.5f, 2, 1, 1);
 		trigterShallowThresholdUnitLabel.Text = "%";
 		trigterDeepThresholdTitleLabel = CreateControl<Label>(inputConfigurationGroupBox, 0.5f, 3, 9, 1);
-		trigterDeepThresholdTitleLabel.Text = uiText.InputTriggerThresholdDeep;
 		trigterDeepThresholdNumericUpDown = CreateControl<NumericUpDown>(inputConfigurationGroupBox, 9.5f, 3, 4, 1);
 		trigterDeepThresholdNumericUpDown.Maximum = 100;
 		trigterDeepThresholdNumericUpDown.Minimum = 30;
@@ -164,7 +166,6 @@ class Stick3020: Form
 		trigterDeepThresholdUnitLabel = CreateControl<Label>(inputConfigurationGroupBox, 13.5f, 3, 1, 1);
 		trigterDeepThresholdUnitLabel.Text = "%";
 		notchHysteresisTitleLabel = CreateControl<Label>(inputConfigurationGroupBox, 0.5f, 4, 9, 1);
-		notchHysteresisTitleLabel.Text = uiText.InputNotchHysteresis;
 		notchHysteresisNumericUpDown = CreateControl<NumericUpDown>(inputConfigurationGroupBox, 9.5f, 4, 4, 1);
 		notchHysteresisNumericUpDown.Maximum = 100;
 		notchHysteresisNumericUpDown.Minimum = 0;
@@ -173,10 +174,41 @@ class Stick3020: Form
 		notchHysteresisUnitLabel = CreateControl<Label>(inputConfigurationGroupBox, 13.5f, 4, 1, 1);
 		notchHysteresisUnitLabel.Text = "%";
 		enableDeadmanCheckBox = CreateControl<CheckBox>(inputConfigurationGroupBox, 0.5f, 5, 14, 1);
-		enableDeadmanCheckBox.Text = uiText.InputDeadman;
 		inputConfigurationGroupBox.ResumeLayout();
 
 		mainPanel.ResumeLayout();
 		ResumeLayout();
+
+		SetControlTexts();
+	}
+
+	private void SetControlTexts()
+	{
+		controllerSelectGroupBox.Text = uiText.ControllerSelect;
+		for (int i = 0; i < 4; i++)
+		{
+			controllerSelectRadioButtons[i].Text = i.ToString();
+		}
+
+		brakeSelectGroupBox.Text = uiText.BrakeSelect;
+		brakeAutoRadioButton.Text = string.Format("{0}：{1}", uiText.BrakeAuto, uiText.Brake6StepForAuto);
+		brake6StepRadioButton.Text = uiText.Brake6Step;
+		brake7StepRadioButton.Text = uiText.Brake7Step;
+		brake8StepRadioButton.Text = uiText.Brake8Step;
+		brakeAnalogRadioButton.Text = uiText.BrakeAnalog;
+
+		operationStatusGroupBox.Text = uiText.OperationStatus;
+		powerTitleLabel.Text = uiText.OperationPower;
+		powerDepthLabel.TextAlign = ContentAlignment.TopRight;
+		brakeTitleLabel.Text = uiText.OperationBrake;
+		leftTriggerTitleLabel.Text = uiText.OperationLeftTrigger;
+		rightTriggerTitleLabel.Text = uiText.OperationRightTrigger;
+
+		inputConfigurationGroupBox.Text = uiText.InputConfiguration;
+		stickThresholdTitleLabel.Text = uiText.InputStickThreshold;
+		trigterShallowThresholdTitleLabel.Text = uiText.InputTriggerThresholdShallow;
+		trigterDeepThresholdTitleLabel.Text = uiText.InputTriggerThresholdDeep;
+		notchHysteresisTitleLabel.Text = uiText.InputNotchHysteresis;
+		enableDeadmanCheckBox.Text = uiText.InputDeadman;
 	}
 }
